53e2a285d7c8d53be17712fc9ad21c8d7ed87315
Ben Lengerich <blengerich@gmail.com>
2022-07-10 16:56:12 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(name='gam_purification',
+      packages=['gam_purification'],
+      version='0.0.0',
+      install_requires=[
+          'numpy',
+          'scikit-learn',
+          'python-igraph',
+          'matplotlib',
+          'pandas',
+          'tensorflow>=2.4.0',
+          'tensorflow-addons',
+          'numpy>=1.19.2',
+          'ipywidgets',
+      ],
+)
