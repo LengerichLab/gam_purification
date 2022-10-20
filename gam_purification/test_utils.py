8c9e5b41de97ae0cc4d7a9c2ff1204f7a7f2e551
Ben Lengerich <blengerich@gmail.com>
2022-10-20 11:02:10 -0400

    run pyblack

--- gam_purification/test_utils.py
+++ gam_purification/test_utils.py
@@ -10,10 +10,11 @@ class TestMerge(unittest.TestCase):
     def test_merge_arrs(self):
         ar1_names = np.array(list(range(10)))
         ar2_names = ar1_names[::2]
-        ar2_values = np.ones((len(ar2_names), ))
+        ar2_values = np.ones((len(ar2_names),))
         ar2_mapped = merge_arrs(ar1_names, ar2_names, ar2_values)
 
         assert len(ar2_mapped) == len(ar1_names) - 1
+
 
 if __name__ == "__main__":
     unittest.main()
